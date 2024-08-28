7d5b8695a9021cec6dc063ec96d3665294ae5634
김유한 <yuhan6653@gmail.com>
2024-08-28 15:40:44 +0900

    0828-4

--- src/components/body/MainFrontPage.js
+++ src/components/body/MainFrontPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../../assests/styles/body/MainFrontPage.css";
 import MainFrontPageBackgroundImg from "../../assests/images/MainFrontPageBackgroundImg.jpg";
+import ChichizickIcon from "../../assests/images/icons/chichizickIcon.svg";
+import DiscordIcon from "../../assests/images/icons/discordIcon.svg";
 
 function MainFrontPage() {
   return (
@@ -14,11 +16,54 @@ function MainFrontPage() {
           />
         </div>
       </div>
-      <div className="MainFrontPageGridWrap">
-        <div className="MainFrontPageGrid"></div>
+      <div className="mainFrontPageGridWrap">
+        <div className="mainFrontPageGrid">
+          <div className="mainFrontPageHeadTextWrap">
+            <div className="mainFrontPageHeadUpperText">오리더기와</div>
+            <div className="mainFrontPageHeadMiddleText">
+              <div className="mainFrontPageHeadMiddleText1">
+                <span>함께한</span>
+                <span> 순간,</span>
+              </div>
+              <div className="mainFrontPageHeadMiddleText2">
+                <span>더욱</span>
+                <span> 소중한 시간!</span>
+              </div>
+            </div>
+            <div className="mainFrontPageHeadLowerText">
+              <div className="mainFrontPageHeadLowerText1">
+                오리더기의 기타 체널들
+              </div>
+              <div className="mainFrontPageHeadLowerText2">
+                <a href="https://chzzk.naver.com/9d00372c1cec9544f8d54d7914d797b6">
+                  <div className="mainFrontPageHeadLowerText2Area">
+                    <div className="mainFrontPageHeadLowerText2ImgWrap">
+                      <img src={ChichizickIcon} alt="Chichizick Icon"></img>
+                    </div>
+                    <div>보라더기 치치직 채널 바로가기</div>
+                  </div>
+                </a>
+              </div>
+              <div className="mainFrontPageHeadLowerText3">
+                <a href="https://discord.gg/MN7M2C6Yt7">
+                  <div className="mainFrontPageHeadLowerText3Area">
+                    <div className="mainFrontPageHeadLowerText3ImgWrap">
+                      <img src={DiscordIcon} alt="Discord Icon"></img>
+                    </div>
+                    <div>보라더기 디스코드 체널 참가</div>
+                  </div>
+                </a>
+              </div>
+              <div className="mainFrontPageHeadLowerText4">
+                <div className="mainFrontPageHeadLowerText4Area">
+                  오리더기 가입 문의글 작성하러 가기
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    // 123123
   );
 }
 
